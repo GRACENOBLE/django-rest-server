5f1fd413ed344747cdbcdd30dfe925c5daafec3e
GRACENOBLE <gracenoble72@gmail.com>
2024-10-03 17:55:36 +0300

    feat(endpoints): created endpoints for api

--- restbackend/api/views.py
+++ restbackend/api/views.py
@@ -4,8 +4,19 @@ from rest_framework import status
 from .models import Book
 from .serializer import BookSerializer
 
+#endpoint for get
 @api_view(['GET'])
 def get_books(request):
     books = Book.objects.all()
     serialized_data =BookSerializer(books, many = True).data 
     return Response(serialized_data)
+
+# endpoint for post
+@api_view(['POST'])
+def create_book(request):
+    data = request.data
+    serializer = BookSerializer(data = data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
