3a31487f96010c6ab1a988aa423594218e00f1c9
GRACENOBLE <gracenoble72@gmail.com>
2024-10-05 13:21:28 +0300

    feat(endpoint): configured post api endpoint

--- restbackend/api/models.py
+++ restbackend/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    release_date = models.IntegerField()
+    release_date = models.CharField(max_length=50)
     
     def __str__(self):
         return self.title
